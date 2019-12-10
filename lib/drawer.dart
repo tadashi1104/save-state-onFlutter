52b5c26472beded0f75ac9e2dde8c45c3f6b4e1b
tadashi1104 <1tadashi12s9@gmail.com>
2019-12-11 02:07:21 +0900

    20191211-1

---
+++ lib/drawer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:save_state_on_flutter/list_page.dart';
+
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawer createState() => _MyDrawer();
+}
+
+class _MyDrawer extends State<MyDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('save-state',
+                style: Theme.of(context).primaryTextTheme.title),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          ListTile(
+            title: Text("List"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return ListPage();
+                  },
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: Text("Calendar"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+        ],
+      ),
+    );
+  }
+}
